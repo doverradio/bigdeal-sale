e726fdc033c442236fb53981872ccc088ba74b95
Eric <ericlvalencia@gmail.com>
2023-10-09 17:01:13 -0700

    Updated route for tiktok.

--- backend/routes/tiktok.js
+++ backend/routes/tiktok.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+
+const { getTikTokCode } = require('../controllers/tiktok');
+
 router.get('/tiktokaccepted', getTikTokCode)
 
 module.exports = router;
