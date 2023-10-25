63796db6e80d87a641a8cd741017433cab3c58a3
Eric <ericlvalencia@gmail.com>
2023-10-24 21:52:11 -0700

    Updated redirect controller to adjust for prefetch.

--- backend/controllers/redirect.js
+++ backend/controllers/redirect.js
@@ -7,12 +7,15 @@ const log = console.log
 exports.redirectUrl = async (req, res) => {
     const url = await Url.findOne({ urlCode: req.params.urlCode });
 
+
     if (url) {
-        // Check if the request is a pre-fetch
-        if (!req.get('Purpose') === 'prefetch') {
+        console.log(`Found URL: ${url.originalUrl} with clicks: ${url.clicks}`);
+        
+        if (req.get('Purpose') !== 'prefetch') {
             // Increment the click count for this URL
             url.clicks = url.clicks + 1;
             await url.save();
+            console.log(`Updated clicks count: ${url.clicks}`);
         }
 
         res.redirect(url.originalUrl);
